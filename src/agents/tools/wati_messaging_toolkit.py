a6bac32ee143e0be8c178f33d0484a3ca96225d4
nhlanhla <nhlanhla@unlimtd.xyz>
2025-05-29 12:42:03 +0000

    adding wati templates

--- src/agents/tools/wati_messaging_toolkit.py
+++ src/agents/tools/wati_messaging_toolkit.py
@@ -3,23 +3,29 @@ from agno.tools import Toolkit
 import httpx
 
 class WatiMessagingToolkit(Toolkit):
-   API_URL = WatiSettings().MESSAGING_API_URL
-   ID = WatiSettings().MESSAGE_LOG_TYPE_ID
-   
+   M_API_URL = WatiSettings().MESSAGING_API_URL
+   M_ID = WatiSettings().MESSAGE_LOG_TYPE_ID
+   W_API_URL = WatiSettings().MESSAGING_API_URL
+
    def __init__(self):
       super().__init__(name="wati_messaging_toolkit")
       
    def send_template(self, to_field: str, body: str) -> str :
       
       data = {
-         "MessageLogTypeId": self.ID,
+         "MessageLogTypeId": self.M_ID,
          "Subject": "Wati Service",
          "ToField": to_field,
          "Body": body
       }
       
-      response = httpx.post(f"{self.API_URL}/Messaging/CreateOutboundMail", data=data)
+      response = httpx.post(f"{self.M_API_URL}/Messaging/CreateOutboundMail", data=data)
       return response.json()
+   
+   def get_templates(self) -> str:
+      response = httpx.get(f"{self.W_API_URL}/Wati/GetMessageTemplates")
+      return response.json()
+      
       
       
    
