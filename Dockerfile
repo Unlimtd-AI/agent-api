800dedf4cfa5eb921b20a2229dc32990138b48ae
nhlanhla <nhlanhla@unlimtd.xyz>
2025-06-04 10:59:44 +0000

    ollam,a serve working

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,20 @@
 FROM agnohq/python:3.12
 
+# [Optional] Uncomment this section to install additional OS packages.
+RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
+  && apt-get -y install --no-install-recommends software-properties-common pciutils lshw
+
+RUN curl -fsSL https://ollama.com/install.sh | sh
+
 ARG USER=app
 ARG APP_DIR=/app
 ENV APP_DIR=${APP_DIR}
 
 # Create user and home directory
 RUN groupadd -g 61000 ${USER} \
-  && useradd -g 61000 -u 61000 -ms /bin/bash -d ${APP_DIR} ${USER}
+  && useradd -g 61000 -u 61000 -ms /bin/bash -d ${APP_DIR} ${USER} \
+  && echo "${USER}:1234" | chpasswd \
+  && echo "${USER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 WORKDIR ${APP_DIR}
 
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,25 @@
 # Container Entrypoint script
 ############################################################################
 
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# echo "Serve Ollama"
+sudo nohup ollama serve > /tmp/ollama.log 2>&1 &
+
+echo "Pull Models"
+# # For chat
+ollama pull deepseek-r1:8b
+ollama pull deepseek-r1:70b
+
+# For tools
+ollama pull llama3.1:8b
+ollama pull llama3.1:70b
+
+# For embedding
+ollama pull llama2:7b
+ollama pull llama2:70b
+
 if [[ "$PRINT_ENV_ON_LOAD" = true || "$PRINT_ENV_ON_LOAD" = True ]]; then
   echo "=================================================="
   printenv
@@ -28,28 +47,6 @@ case "$1" in
     exec "$@"
     ;;
 esac
-
-curl -fsSL https://ollama.com/install.sh | sh
-
-# Add to PATH for both root and vscode user
-PATH="$HOME/.local/bin:${PATH}"
-
-nohup ollama serve > /tmp/ollama.log 2>&1 &
-
-echo "Pull Models"
-sleep 2
-
-# For chat
-ollama pull deepseek-r1:8b
-ollama pull deepseek-r1:70b
-
-# For tools
-ollama pull llama3.1:8b
-ollama pull llama3.1:70b
-
-# For embedding
-ollama pull llama2:7b
-ollama pull llama2:70b
 
 echo ">>> Hello World!"
 
